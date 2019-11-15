5efd825bfd7aa3b246cf7f6e9759626c543e8c75
Paulo Diogo Ribeiro <paulo.d.ribeiro@inesctec.pt>
2019-11-15 17:56:12 +0000

    figured out template explicit specialization

--- src/robin/robin_inst.cpp
+++ src/robin/robin_inst.cpp
@@ -5,20 +5,6 @@
 #include "robin/TestStruct_foo.h"
 #include "std_msgs/Float64.h"
 template class RobinPublisher<double, std_msgs::Float64>;
-template class RobinPublisher<TestStruct_foo, robin::TestStruct_foo>;
+// template class RobinPublisher<TestStruct_foo, robin::TestStruct_foo>;
 template class RobinSubscriber<double, std_msgs::Float64>;
-template class RobinSubscriber<TestStruct_foo, robin::TestStruct_foo>;
-// template<> class RobinPublisher<double, std_msgs::Float64>
-// {
-//     void read()
-//     {
-//         this->shared_memory_.read(&msg_);  //TRY without 'this->'
-//     }
-// };
-// template<> class RobinSubscriber<double, std_msgs::Float64>
-// {
-//     void write(std_msgs::Float64 *msg)
-//     {
-//         this->shared_memory_.write(msg);  //TRY without 'this->'
-//     }
-// };
+// template class RobinSubscriber<TestStruct_foo, robin::TestStruct_foo>;
--- src/robin/shared_memory_inst.cpp
+++ src/robin/shared_memory_inst.cpp
@@ -2,5 +2,29 @@
 #include "robin/structs.h"
 #include "robin/TestStruct_foo.h"
 #include "std_msgs/Float64.h"
+template<> void SharedMemory<double, std_msgs::Float64>::read(std_msgs::Float64 *msg_ptr)
+{
+  if (!isOpen())
+  {
+    ROS_ERROR("Shared memory '%s' is not open.", name_.c_str());
+    throw 2;
+  }
+  semaphore_.wait();
+  msg_ptr->data = *shm_ptr_;
+  printf("read\n");
+  semaphore_.post();
+};
+template<> void SharedMemory<double, std_msgs::Float64>::write(std_msgs::Float64 const *msg_ptr)
+{
+  if (!isOpen())
+  {
+    ROS_ERROR("Shared memory '%s' is not open.", name_.c_str());
+    throw 2;
+  }
+  semaphore_.wait();
+  *shm_ptr_ = msg_ptr->data;
+  printf("written\n");
+  semaphore_.post();
+};
 template class SharedMemory<double, std_msgs::Float64>;
 template class SharedMemory<TestStruct_foo, robin::TestStruct_foo>;
